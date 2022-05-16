118dac280a9d638b2c60b3d4b119868abdcb7915
IT21346668 <99562180+IT21346668@users.noreply.github.com>
2022-05-16 16:15:56 +0530

    category.cpp

---
+++ category.cpp
@@ -0,0 +1,9 @@
+void category::display_categorized_apps(){
+   //implementation goes here
+}
+void category::add_categories(int id,string catName){
+  //implementation goes here  
+}
+ void category::update_categories(){
+     //implementation goes here
+ }
