d68bd84eede821763dbff23dc8b93519bb75f8b6
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-16 16:24:22 +0530

    Update main cpp with user objects

--- main.cpp
+++ main.cpp
@@ -10,12 +10,21 @@ int ID;
 	developer* dev1, * dev2;
 	dev1 = new developer;
 	dev2 = new developer;
+	
+	//Creating Dynamic User Objects
+	user* us1, * us2;
+	us1 = new user;
+	us2 = new user;
 
-  //Initializing Developers
+  	//Initializing Developers
 	dev1->set_developer(ID,CompanyName,Email,Password,About);
 	dev2->set_developer(ID, CompanyName, Email, Password, About);
+	
+	//Initializing Users
+	us1->set_user_details(Email, FirstName, LastName, Password, Dob, Country);
+	us2->set_user_details(Email, FirstName, LastName, Password, Dob, Country);
 
-  //deleting dynamic objects
-  delete dev1, dev2;
-
+  	//deleting dynamic objects
+  	delete dev1, dev2;
+  	delete us1, us2;
 }
