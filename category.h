e6508222f1e439289bd4467fb93aaf38e20e6907
IT21346668 <99562180+IT21346668@users.noreply.github.com>
2022-05-16 16:13:20 +0530

    category.h

---
+++ category.h
@@ -0,0 +1,9 @@
+class category {
+    private:
+        int category_id;
+        string category_name;
+    public:
+        void display_categorized_apps();
+        void add_categories(int id,string catName);
+        void update_categories();
+};
