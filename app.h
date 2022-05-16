aa0e08ecc4df13df82fbcd220ceafce3b76e0aa2
Deneth Pinsara <99629453+IT21158186@users.noreply.github.com>
2022-05-16 17:09:30 +0530

    Update app.h

--- app.h
+++ app.h
@@ -5,7 +5,7 @@ string app_name;
 string description;
 string file_path;
 public:
-void app(int Id,string Name,string desc,string fpath);
+app(int Id,string Name,string desc,string fpath);
 void update_app_details();
 void get_app_details();
 }
