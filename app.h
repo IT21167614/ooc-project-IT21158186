30b004c2de1fec6bcae6874c4280dbb72a7fbcec
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:04:56 +0530

    Update app.h

--- app.h
+++ app.h
@@ -1,11 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
 class app {
+
 private:
-int app_id;
-string app_name;
-string description;
-string file_path;
+	int app_id;
+	string app_name;
+	string description;
+	string file_path;
 public:
-app(int Id,string Name,string desc,string fpath);
-void update_app_details();
-void get_app_details();
-}
+	app(int Id, string Name, string desc, string fpath);
+	void update_app();
+	void get_app_details();
+};
