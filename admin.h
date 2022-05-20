6543eec002457f6c981233ec54af3f95e463f855
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:04:10 +0530

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,11 +1,14 @@
-}
-class admin{
+#pragma once
+#include <string>
+using namespace std;
+class admin {
+
 private:
-  int admin_id;
-  string admin_name;
-  string email;
-  string password;
+	int admin_id;
+	string admin_name;
+	string email;
+	string password;
 public:
-  admin(int id, string name, string email, string pwd);
-  void update_admin();
+	admin(int id, string name, string email, string pwd);
+	void update_admin();
 };
