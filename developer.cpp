eea678648c2bda2b8f77651bf5595758a3eb6f65
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:08:01 +0530

    Update developer.cpp

--- developer.cpp
+++ developer.cpp
@@ -1,15 +1,16 @@
+#include "developer.h"
 void developer::update_dev_details() {
-  //implementation goes here
+    //implementation goes here
 }
-
 void developer::upload_app() {
-  //implementation goes here
+    //implementation goes here
 }
-
 void developer::update_app() {
-  //implementation goes here
+    //implementation goes here
 }
-
 void developer::reply_review() {
-  //implementation goes here
+    //implementation goes here
+}
+developer::developer(int devId, string companyName, string mail, string pwd, string About) {
+    //implementation goes here
 }
