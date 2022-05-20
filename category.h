a624e43e5c3d88146be341cda5c5b7cadd0780ce
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:06:46 +0530

    Update category.h

--- category.h
+++ category.h
@@ -1,9 +1,14 @@
+#pragma once
+#include <string>
+using namespace std;
 class category {
-    private:
-        int category_id;
-        string category_name;
-    public:
-        void display_categorized_apps();
-        void add_categories(int id,string catName);
-        void update_categories();
+
+private:
+	int category_id;
+	string category_name;
+public:
+	void display_categorized_apps();
+	void add_categories(int id, string catName);
+	void update_categories();
 };
+
