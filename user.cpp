788ffe3eef3e48983b38258054082ab4cdd90eb7
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:11:03 +0530

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,12 +1,14 @@
-void user::set_user_details(string mail,string fname,string lname,string pwd,string dob,string ctry){
-    //implementation goes here
+#include "user.h"
+void user::set_user_details(string mail, string fname, string lname, string pwd, string dob, string ctry) {
+	//implementation goes here
 }
-int user::cal_age(){
-    //implementation goes here
+int user::cal_age() {
+	//implementation goes here
+	return 0;
 }
-void user::update_profile(){
-    //implementation goes here
+void user::update_profile() {
+	//implementation goes here
 }
-void user::write_review(){
-    //implementation goes here
+void user::write_review() {
+	//implementation goes here
 }
