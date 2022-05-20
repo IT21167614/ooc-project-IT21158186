8ea2a589848f9f8c99031b5dfc9821a96e42536e
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:03:20 +0530

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,6 +1,7 @@
-admin::admin(int id, string name, string email, string pwd){
-//implementation goes here
+#include "admin.h"
+admin::admin(int id, string name, string email, string pwd) {
+	//implementation goes here
 }
-void admin::update_admin(){
-//implementation goes here
+void admin::update_admin() {
+	//implementation goes here
 }
