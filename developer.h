b4ec366bb630e38c761c3bf6fa31f7a165a44521
Dilruk Nimsara Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-05-21 01:07:25 +0530

    Update developer.h

--- developer.h
+++ developer.h
@@ -1,4 +1,8 @@
+#pragma once
+#include <string>
+using namespace std;
 class developer {
+
 private:
 	int developer_id;
 	string company_name;
@@ -6,9 +10,10 @@ private:
 	string password;
 	string about;
 public:
-	void set_developer(int devId,string companyName,string mail,string pwd,string About);
+	developer(int devId, string companyName, string mail, string pwd, string About);
 	void update_dev_details();
 	void upload_app();
 	void update_app();
 	void reply_review();
 };
+
